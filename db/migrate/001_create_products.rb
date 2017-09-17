2b5b40caffb69cee07bdaccd2a94f05d5bffaa1f
Mikhail Kruhlik <miheykrug@mail.ru>
2017-09-17 19:33:16 +0000

    Create plugin Products

--- db/migrate/001_create_products.rb
+++ db/migrate/001_create_products.rb
@@ -6,7 +6,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table  :products do |t|
       t.string    :name
-      t.string    :tipe
+      t.string    :type
       t.integer   :amount, :default => 0
       t.string    :measure
       t.integer   :price,  :default => 0
